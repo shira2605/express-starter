22259172dc4b37376ff6dec236ca3b32a4557566
Shira Benhamu <shira.2605@berkeley.edu>
2014-02-26 00:52:00 -0800

    Button

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -9,11 +9,15 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  //WRITE CODE HERE
+  $('#div').hide('Write some code to make this paragraph disappear when you click on the button. Then write some code to make it reappear with the other button');
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+  $('#div').show('Write some code to make this paragraph disappear when you click on the button. Then write some code to make it reappear with the other button');
+});
+
+$('#color').click(function() {
+	$('#color').css('color','pink');
 });
 
 
