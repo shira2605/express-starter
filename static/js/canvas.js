3a3561c2cc0ded7885042c54258d144acceaa639
Shira Benhamu <shira.2605@berkeley.edu>
2014-03-12 19:22:20 -0700

    colores

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -39,7 +39,29 @@ $('#p5').click(function() {
 	context.moveTo(50, 50);
 	context.lineTo(283, 370);
 	context.closePath();
-	context.stroke()
+	context.stroke();
+});
+
+ $('#p6').click(function() {
+ 	context.strokeStyle ='green';
+    context.strokeRect(10, 10, 200, 400);
+});
+
+$('#p7').click(function() {
+	context.beginPath();
+	context.strokeStyle='red';
+	context.fillStyle='red';
+	context.arc(150, 150, 100, 0, 2*Math.PI);
+	context.closePath();
+	context.fill();
+
+});
+
+ $('#p8').click(function() {
+    context.strokeStyle='blue';
+    context.fillStyle='yellow';
+    context.strokeRect(10, 10, 200, 200);
+    context.fillRect(10, 10, 200, 200);
 });
 
 });
