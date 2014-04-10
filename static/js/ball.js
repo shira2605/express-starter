4a9a1b0346904f3576e6498843f7da24f45bbb61
Shira Benhamu <shira.2605@berkeley.edu>
2014-04-09 19:51:09 -0700

    segunda parte de la pelota

--- static/js/ball.js
+++ static/js/ball.js
@@ -8,10 +8,18 @@ $(document).ready(function() {
   //PUT STUFF HERE
 
   //run an iteration of the game
-var x_above = 20
+var ball = {
+  x_above: 20,
+  y_above: 20,
+  radius: 20,
+  vy: 5,
+  vx: 5,
+
+};
 
   var updateGame = function() {
-    x_above = x_above + 5
+    ball.x_above = ball.x_above + ball.vx
+    ball.y_above = ball.y_above + ball.vy
     context.strokeStyle='white';
     context.fillStyle='white';
     context.strokeRect(0, 0, 800, 600);
@@ -19,9 +27,22 @@ var x_above = 20
     context.beginPath();
     context.strokeStyle='pink';
     context.fillStyle='pink';
-    context.arc(x_above, 20, 20, 0, 2*Math.PI);
+    context.arc(ball.x_above, ball.y_above, ball.radius, 0, 2*Math.PI);
     context.closePath();
     context.fill()
+    if (ball.vy > 0 && ball.y_above >= 600) {
+      ball.vy = -5;
+    }
+    if (ball.vx > 0 && ball.x_above >= 800) {
+      ball.vx = -5;
+    }
+    if (ball.vy < 0 && ball.y_above <= 0) {
+      ball.vy = 5;
+    }
+    if (ball.vx < 0 && ball.x_above <= 0) {
+      ball.vx = 5;
+    }
+
     setTimeout(updateGame, 10);
     //PUT STUFF HERE
   };
