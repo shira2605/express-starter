75d431e6cf9d494c76aba15e64f3f0980f2b9fcf
Shira Benhamu <shira.2605@berkeley.edu>
2014-04-30 19:52:41 -0700

    juego completo sin niveles

--- static/js/chainr.js
+++ static/js/chainr.js
@@ -23,12 +23,22 @@ balls.push(b);
 reactions = []; 
 var reacting = false 
 var numReacted = 0
+var gameState = 'menu'
+var menuText = "Click to Play!"
+
 
   var updateGame = function() {
     context.strokeStyle='white';
     context.fillStyle='white';
     context.strokeRect(0, 0, 800, 600);
     context.fillRect(0, 0, 800, 600);
+    if (gameState === 'menu') {
+      context.fillStyle = 'lightgreen';
+      context.font = "50px Comic Sans"; 
+      context.fillText(menuText, 50, 300);
+    }
+    else if (gameState === 'playing') {
+    
     for (var i = 0; i < balls.length; i++) {
     balls[i].x_above = balls[i].x_above + balls[i].vx
     balls[i].y_above = balls[i].y_above + balls[i].vy
@@ -97,12 +107,18 @@ var numReacted = 0
             numReacted = numReacted + 1;  
         }
       }
+       context.fillStyle = "lightgreen"; 
+    context.font = "20px Comic Sans";
+    context.fillText('Reactions:' + numReacted , 1, 50);
+    }
+  if ( reacting === true && reactions.length === 0) {
+    menuText = 'Game Over:  You Reacted ' + numReacted + ' balls!';
+    gameState = 'menu';
+  } 
 
     requestAnimationFrame(updateGame);
     //PUT STUFF HERE
-    context.fillStyle = "lightgreen"; 
-    context.font = "20px Comic Sans";
-    context.fillText('Reactions:' + numReacted , 1, 50); 
+    
   }
  
 
@@ -110,7 +126,23 @@ var numReacted = 0
 
   // Handle a canvas click event
   $('#game_canvas').click(function(e) {
-  if (reacting === false) {
+    if (gameState === 'menu') {
+      gameState = 'playing'
+      reacting = false
+      numReacted = 0
+      balls = []
+      for (var i = 0; i< numBalls; i++) {
+          var b = {
+          x_above: 100,
+  y_above: 20,
+  radius: 20,
+  vy: 10*Math.random(),
+  vx: 10*Math.random(),
+};
+balls.push(b);
+} 
+    }
+  else if (reacting === false && gameState === 'playing') {
     // Find the mouse x and y relative to the top-left corner of the canvas
     var x = e.pageX - $(this).offset().left;
     var y = e.pageY - $(this).offset().top;
